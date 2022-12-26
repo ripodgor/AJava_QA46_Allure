8cb680ac2a7a562db237162ac4a6b90f8087024d
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-12-26 15:14:02 +0300

    Updated files

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,17 +1,23 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.SelenideElement;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.google.j2objc.annotations.LoopTranslation;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
 
-import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,6 +26,16 @@ class CardDeliveryTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
